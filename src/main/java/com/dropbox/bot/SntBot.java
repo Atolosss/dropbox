86920e44aefe29b6d092d5024c86c40ae0301f26
tsm.komarov <tsm.komarov@gmail.com>
2024-01-02 15:58:13 +0300

    bot

---
+++ src/main/java/com/dropbox/bot/SntBot.java
@@ -0,0 +1,69 @@
+package com.dropbox.bot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class SntBot extends TelegramLongPollingBot {
+    private static final Logger LOG = LoggerFactory.getLogger(SntBot.class);
+    private static final String BEGIN = "/start";
+
+    public SntBot(@Value("${telegram.bot.token}") final String botToken) {
+        super(botToken);
+    }
+
+    @Override
+    public void onUpdateReceived(final Update update) {
+        if (!update.hasMessage() || update.getMessage().hasText()) {
+            return;
+        }
+        var message = update.getMessage().getText();
+        var chatId = update.getMessage().getChatId();
+
+        switch (message) {
+            case BEGIN -> {
+                final String userName = update.getMessage().getChat().getUserName();
+                startCommand(chatId, userName);
+            }
+            default -> System.out.println("xnj nj nj nj");
+        }
+    }
+
+    private void startCommand(final Long chatId, final String userName) {
+        var text = """
+            Добро пожаловать в бот, %s!
+
+            Здесь Вы сможете сообщить обо всех неисправностях или пожеланиях приложив фотограции.
+
+            Для этого воспользуйтесь командами:
+            /usd - курс доллара
+            /eur - курс евро
+
+            Дополнительные команды:
+            /help - получение справки
+            """;
+        var formattedText = String.format(text, userName);
+        sendMessage(chatId, formattedText);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "komarov_cnt_bot";
+    }
+
+    private void sendMessage(final Long chatId, final String text) {
+        var chatIdStr = String.valueOf(chatId);
+        var sendMessage = new SendMessage(chatIdStr, text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            LOG.error("Ошибка отправки сообщения", e);
+        }
+    }
+}
---
+++ src/main/java/com/dropbox/configuration/SntBotConfiguration.java
@@ -0,0 +1,19 @@
+package com.dropbox.configuration;
+
+import com.dropbox.bot.SntBot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class SntBotConfiguration {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(final SntBot sntBot) throws TelegramApiException {
+        var api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(sntBot);
+        return api;
+    }
+}
